be3594f116a98d115f8f14bf96e10a568d40e97d
Aanvi262 <73943879+Aanvi262@users.noreply.github.com>
2021-10-28 13:08:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,96 @@
+function time()
+{
+    window.location.replace("time.html");
+}
+function money()
+{
+    window.location.replace("money.html");
+}
+function home()
+{
+    window.location.replace("index.html");
+}
+function skip_counting()
+{
+    window.location.replace("skipcounting.html");
+}
+     
+function multidigit_add()
+{
+    window.location.replace("multidigitadd.html");
+}
+function multidigitsub()
+{
+    window.location.replace("multidigitsub.html");
+}
+
+function measurment()
+{
+    window.location.replace("measurment.html");
+}
+function placevalue()
+{
+    window.location.replace("placevalue.html");
+}
+function data()
+{
+    window.location.replace("data.html");
+}
+function probability()
+{
+    window.location.replace("probability.html");
+}
+function shapes()
+{
+    window.location.replace("shapes.html");
+}
+function rounding()
+{
+    window.location.replace("rounding.html");
+}
+function multiplication()
+{
+    window.location.replace("multiplication.html");
+}
+function fractions()
+{
+    window.location.replace("fractions.html");
+}
+function books()
+{
+    window.location.replace("books.html");
+}
+function math()
+{
+    window.location.replace("math.html");
+}
+function fantasy()
+{
+    window.location.replace("fantasy.html");
+}
+function realist()
+{
+    window.location.replace("realist.html");
+}
+function sci()
+{
+    window.location.replace("scifi.html");
+}
+function com()
+{
+    window.location.replace("comedy.html");
+}
+function mathunits()
+{
+    window.location.replace("math.html");
+}
+function genres()
+{
+    window.location.replace("books.html");
+}
+function histor()
+{
+    window.location.replace("history.html");
+}
+
+
